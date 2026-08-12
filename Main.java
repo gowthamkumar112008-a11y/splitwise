a72ffb5bbd37d0e8f8045dd54ed587fc911f0ad1
gowtham <gowthamkumar112008@gmail.com>
2026-08-12 09:29:50 +0530

    1.1

---
+++ Main.java
@@ -0,0 +1,14 @@
+package splitwise;
+
+
+public class Main {
+    void method() {
+        System.out.println("welcome");
+    }
+    public static void main(String[] args) {
+        Main obj= new Main();
+        obj.method();
+
+        }
+    }
+}
